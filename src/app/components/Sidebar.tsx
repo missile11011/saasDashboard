3caaf6af93224a185bf6df89072b28d0b39ce31b
Misael Reyes <misaelrey90@gmail.com>
2024-11-15 17:09:54 -0600

    Invalid DOM property stroke-linecap, and stroke-linejoin fixed

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,36 +1,13 @@
 export default function Sidebar() {
 	return (
-		<div>
-			<button
-				data-drawer-target="default-sidebar"
-				data-drawer-toggle="default-sidebar"
-				aria-controls="default-sidebar"
-				type="button"
-				className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-			>
-				<span className="sr-only">Open sidebar</span>
-				<svg
-					className="w-6 h-6"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						clip-rule="evenodd"
-						fill-rule="evenodd"
-						d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-					></path>
-				</svg>
-			</button>
+		<div className="">
 
 			<aside
 				id="default-sidebar"
-				className="  w-64 transition-transform -translate-x-full sm:translate-x-0"
+				className="h-screen w-52 bg-gray-50 dark:bg-neutral-700 transition-transform -translate-x-full sm:translate-x-0 top-0"
 				aria-label="Sidebar"
-			>	
-				
-				<div className="h-full px-3 py-4 overflow-y-auto w-52 bg-gray-50 dark:bg-neutral-700">
+			>
+				<div className=" px-3 py-4 ">
 					<ul className="space-y-2">
 						<li>
 							<a
@@ -146,8 +123,8 @@ export default function Sidebar() {
 								>
 									<path
 										stroke="currentColor"
-										stroke-linecap="round"
-										stroke-linejoin="round"
+										strokeLinecap="round"
+										strokeLinejoin="round"
 										strokeWidth="2"
 										d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
 									/>
